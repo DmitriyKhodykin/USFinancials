2be6a0da57ef704693b630ff9dd9ee7893b9e260
DmitriyKhodykin <d.hodikin@yandex.ru>
2021-06-15 16:57:38 +0300

    Models tuning module added

---
+++ models/models_tuning.py
@@ -0,0 +1,57 @@
+"""
+Module for tuning model parameters.
+"""
+import json
+
+import pandas
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
+
+from models.models_features import FeaturesImportance
+from models.models_review import review_classification
+from settings import config, params
+from settings.config import reports, target_cols
+from settings.params import models_params, MAIN_SCORE
+
+
+def train_cv() -> dict:
+    """
+    Returns the results of training the model for cross-validation,
+    the classes of features are balanced before being fed to the input of the model.
+    :return: best score, best params
+    """
+    dataframe = pandas.read_parquet(reports['FeaturesData'])
+
+    # Balancing classes
+    sss = StratifiedShuffleSplit(n_splits=5, random_state=config.RANDOM_SEED)
+
+    best_model_name = review_classification()
+    best_model = params.models_dict[best_model_name]
+
+    imp = FeaturesImportance()
+    best_cols_list = imp.evaluate_importance()
+    x_best = dataframe[best_cols_list]
+    y = dataframe[target_cols[0]]
+
+    # Cross-validation on n-folds, enumeration of the best parameters
+    estimator = GridSearchCV(
+        estimator=best_model,
+        param_grid=models_params[best_model_name],
+        scoring=MAIN_SCORE,
+        cv=sss,
+        return_train_score=True
+    )
+    # Fitting
+    estimator.fit(x_best, y)
+
+    # Fitting outcomes - vocabulary with lists of characteristics
+    results = estimator.cv_results_
+    print('Results:', results)
+    print('Best Score:', estimator.best_score_)
+    print('Best Params:')
+    print(json.dumps(estimator.best_params_, indent=4, sort_keys=True))
+
+    return results
+
+
+if __name__ == '__main__':
+    train_cv()
--- settings/params.py
+++ settings/params.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LogisticRegression
 from settings import config
 
 FEATURES_IMPORTANT_RATE = 2.5
+MAIN_SCORE = 'f1'
 
 models_dict = {
     'CatBoost': CatBoostClassifier(
@@ -24,4 +25,76 @@ models_dict = {
     'LogisticRegression': LogisticRegression(
         random_state=config.RANDOM_SEED
     )
+}
+
+models_params = {
+    'CatBoost': {
+        'iterations': [1500, 2000],
+        'learning_rate': [0.3, 0.4],
+        'l2_leaf_reg': [3, 5],
+        'bagging_temperature': [1],
+        'random_strength': [1],
+        'one_hot_max_size': [2],
+        'leaf_estimation_method': ['Newton']
+    },
+    'LGBM': {
+        'boosting_type': ['gbdt'],
+        'num_leaves': [31],
+        'max_depth': [- 1],
+        'learning_rate': [0.1, 0.3],
+        'n_estimators': [100, 500],
+        'subsample_for_bin': [200000],
+        'objective': [None],
+        'class_weight': [None],
+        'min_split_gain': [0.0],
+        'min_child_weight': [0.001],
+        'min_child_samples': [20],
+        'subsample': [1.0],
+        'subsample_freq': [0],
+        'colsample_bytree': [1.0],
+        'reg_alpha': [0.0],
+        'reg_lambda': [0.0],
+        'random_state': [None],
+        'n_jobs=': [1],
+        'silent': [True],
+        'importance_type': ['split']
+    },
+    'RandomForest': {
+        'n_estimators': [10, 100, 1000],
+        'criterion': ['gini'],
+        'max_depth': [None, 10, 100],
+        'min_samples_split': [2, 4, 8],
+        'min_samples_leaf': [1, 3],
+        'min_weight_fraction_leaf': [0.0],
+        'max_features': ['sqrt'],
+        'max_leaf_nodes': [None],
+        'min_impurity_decrease': [0.0],
+        'min_impurity_split': [None],
+        'bootstrap': [True],
+        'oob_score': [False],
+        'n_jobs': [-1],
+        'random_state': [config.RANDOM_SEED],
+        'verbose': [0],
+        'warm_start': [False],
+        'class_weight': [None],
+        'ccp_alpha': [0.0],
+        'max_samples': [None]
+    },
+    'LogisticRegression': {
+        'penalty': ['l2'],
+        'dual': [False],
+        'tol': [0.0001],
+        'C': [1.0],
+        'fit_intercept': [True],
+        'intercept_scaling': [1],
+        'class_weight': [None],
+        'random_state': [config.RANDOM_SEED],
+        'solver': ['lbfgs'],
+        'max_iter': [100],
+        'multi_class': ['auto'],
+        'verbose': [0],
+        'warm_start': [False],
+        'n_jobs': [None],
+        'l1_ratio': [None]
+    }
 }
