11ab16db62fef80c367fb4f93c206880ef0f8f3f
DmitriyKhodykin <d.hodikin@yandex.ru>
2021-06-17 15:43:56 +0300

    Class Model created

--- models/main.py
+++ models/main.py
@@ -3,20 +3,58 @@ Main module for creating serialized model
 """
 import pandas
 from sklearn.metrics import f1_score
-from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV
 
-from services.utils import hold_out
+from services.utils import hold_out, split_data
 from settings import config, params
 from settings.config import reports
 
 
-class FeaturesImportance:
+class Model:
 
     def __init__(self):
-        self.dataframe = pandas.read_parquet(reports['FeaturesData'])
+        self.dataframe = pandas.read_parquet(reports['FeaturesData']).head(1000)
+        self.x, self.y = split_data(self.dataframe)
         self.x_train, self.x_test, self.y_train, self.y_test = hold_out(self.dataframe)
+        self.best_model_name = None  # A variable to store the name of the best model
 
-    def evaluate_importance(self) -> list:
+    def save_best_model(self):
+        pass
+
+    def train_cv(self) -> dict:
+        """
+        Returns the results of training the model for cross-validation,
+        the classes of features are balanced before being fed to the input of the model.
+        :return: best score, best params
+        """
+        # Balancing classes
+        sss = StratifiedShuffleSplit(n_splits=5, random_state=config.RANDOM_SEED)
+
+        # Selection of the best model
+        best_model = params.models_dict[self.best_model_name]
+
+        # Selection of the most important features for the model
+        best_cols_list = self.evaluate_features_importance()
+        x_best = self.x[best_cols_list]
+
+        # Cross-validation on n-folds, enumeration of the best parameters
+        estimator = GridSearchCV(
+            estimator=best_model,
+            param_grid=params.models_params[self.best_model_name],
+            scoring=params.MAIN_SCORE,
+            cv=sss,
+            return_train_score=True
+        )
+
+        # Fitting
+        estimator.fit(x_best, self.y)
+
+        # Fitting outcomes - vocabulary with lists of characteristics
+        print('Best Score:', estimator.best_score_)
+        print('Best Params:', estimator.best_params_)
+        return estimator.best_params_
+
+    def evaluate_features_importance(self) -> list:
         """
         Evaluates the most significant features of the model.
         :return: List with the best features
@@ -24,8 +62,7 @@ class FeaturesImportance:
         # List for best features from features importance
         best_features_indexes: list = []
 
-        best_model_name = review_classification()
-        best_model = params.models_dict[best_model_name]
+        best_model = params.models_dict[self.best_model_name]
         best_model.fit(self.x_train, self.y_train)
         features_importance = best_model.feature_importances_ * 100
 
@@ -39,87 +76,37 @@ class FeaturesImportance:
         print(best_cols_list)
         return best_cols_list
 
+    def review_classification(self) -> str:
+        """
+        Reviews some models at standard settings and compares the results.
+        :return: Best model name
+        """
+        # Models dict from settings.params
+        models = params.models_dict
 
-def train_cv() -> dict:
-    """
-    Returns the results of training the model for cross-validation,
-    the classes of features are balanced before being fed to the input of the model.
-    :return: best score, best params
-    """
-    dataframe = pandas.read_parquet(reports['FeaturesData']).head(1000)
+        results = pandas.DataFrame(columns=['Name', 'F1Score'])
 
-    # Balancing classes
-    sss = StratifiedShuffleSplit(n_splits=5, random_state=config.RANDOM_SEED)
+        for name, model_ in models.items():
+            model_.fit(self.x_train, self.y_train)
+            y_predict = model_.predict(self.x_test)
+            score = f1_score(self.y_test, y_predict)
 
-    # Selection of the best model
-    best_model_name = review_classification()
-    best_model = params.models_dict[best_model_name]
-
-    # Selection of the most important features for the model
-    imp = FeaturesImportance()
-    best_cols_list = imp.evaluate_importance()
-    x_best = dataframe[best_cols_list]
-
-    y = create_binary_target(
-        dataframe,
-        config.target_cols[0],
-        config.CUT_OFF_VALUE
-    )
-
-    # Cross-validation on n-folds, enumeration of the best parameters
-    estimator = GridSearchCV(
-        estimator=best_model,
-        param_grid=models_params[best_model_name],
-        scoring=MAIN_SCORE,
-        cv=sss,
-        return_train_score=True
-    )
-
-    # Fitting
-    estimator.fit(x_best, y)
-
-    # Fitting outcomes - vocabulary with lists of characteristics
-    print('Best Score:', estimator.best_score_)
-    print('Best Params:', estimator.best_params_)
-
-    return estimator.best_params_
-
-def review_classification() -> str:
-    """
-    Reviews some models at standard settings and compares the results.
-    :return: Best model name
-    """
-    dataframe = pandas.read_parquet(reports['FeaturesData'])
-    dataframe = dataframe.copy()
-
-    # Hold-out
-    x_train, x_test, y_train, y_test = hold_out(dataframe)
-
-    # Models from
-    models = params.models_dict
-
-    results = pandas.DataFrame(columns=['Name', 'F1Score'])
-
-    for name, model in models.items():
-        model.fit(x_train, y_train)
-        y_predict = model.predict(x_test)
-        score = f1_score(y_test, y_predict)
-
-        tmp_results = pandas.DataFrame(
-            {'Name': [name], 'F1Score': [score]}
+            tmp_results = pandas.DataFrame(
+                {'Name': [name], 'F1Score': [score]}
+            )
+            results = results.append(tmp_results,
+                                     ignore_index=True)
+        sorted_results = results.sort_values(
+            by=['F1Score'],
+            ascending=False
         )
-        results = results.append(tmp_results,
-                                 ignore_index=True)
-
-    sorted_results = results.sort_values(
-        by=['F1Score'],
-        ascending=False
-    )
-    print(sorted_results)
-    best_model_name: str = sorted_results.head(1)['Name'].values[0]
-
-    return best_model_name
+        print(sorted_results)
+        self.best_model_name = sorted_results.head(1)['Name'].values[0]
+        return self.best_model_name
 
 
 if __name__ == '__main__':
-    pass
+    model = Model()
+    model.review_classification()
+    model.evaluate_features_importance()
+    model.train_cv()
